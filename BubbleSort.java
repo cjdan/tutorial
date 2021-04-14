e151db3ee984532a7237052bc6096180c7a59b8b
cjdan <782897310@qq.com>
2021-04-14 23:50:06 +0800

    冒泡排序算法

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,11 +1,11 @@
-package Study;
+package tutorial;
 
 import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {3, 9, -1, 10, -2};
-        int temp = 0;
+        int temp;
 //        //第一次排序
 //        for (int i=0;i< arr .length-1;i++){
 //            if(arr[i]>arr[i+1]){
