241885a64619ade3200b61e775aa21b82f31fd4f
cjdan <782897310@qq.com>
2021-05-26 21:10:18 +0800

    图的创建与深度优先遍历

---
+++ graph/Graph.java
@@ -0,0 +1,130 @@
+package tutorial.graph;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Graph {
+
+    private ArrayList<String> vertextList;//存储顶点集合
+    private int[][] edges;//存储图对应的邻接矩阵
+    private int numOfEdges;//表示边的个数
+    private boolean[] isVisited;//定义数组，记录某个结点是否被访问过
+    public static void main(String[] args) {
+        //测试代码
+        int n = 5;
+        String[] Vertext ={"A","B","C","D","E"};
+        //创建图对象
+        Graph graph = new Graph(n);
+        for(String vertext: Vertext){
+            graph.insertVertex(vertext);
+        }
+        //添加边
+        //A-B,A-C,B-C,B-D,B-E
+        graph.insertEdge(0,1,1);//A-B
+        graph.insertEdge(0,2,1);//A-C
+        graph.insertEdge(1,2,1);//B-C
+        graph.insertEdge(1,3,1);//B-D
+        graph.insertEdge(1,4,1);//B-E
+        //显示矩阵
+        graph.shouGraph();
+        //深度优先遍历
+        System.out.println("深度优先遍历" );
+        graph.dfs();
+
+
+
+    }
+
+    //构造器
+    public Graph(int n){
+        //初始化矩阵与vertextList
+        edges = new int[n][n];
+        vertextList = new ArrayList<String>(n);
+        numOfEdges = 0;//初始化为0
+        isVisited = new boolean[n];
+    }
+    //图中常用的方法
+    //1.返回节点的个数
+    public int getNumOfVertext(){
+        return vertextList.size();
+    }
+    //2.获得边的方法
+    public int getNumOfEdges(){
+        return numOfEdges;
+    }
+    //3.返回节点i对应的数据,0->A,1->B,2->C
+    public String getValueByIndex(int i){
+        return vertextList.get(i);
+    }
+    //4.返回v1与v2的权值
+    public int getWeight(int v1, int v2){
+        return edges[v1][v2];
+    }
+    //5.显示图对应的矩阵
+    public void shouGraph(){
+        for (int[] link:edges){
+            System.out.println(Arrays.toString(link));
+        }
+    }
+    //得到第一个邻接结点的下标w
+    //如果存在就返回对应的下标，否则返回-1；
+    public int getFirstNeighbor(int index){
+        for(int j = 0;j < vertextList.size();j++){
+            if(edges[index][j] > 0){
+                return j;
+            }
+        }
+        return -1;
+    }
+    //根据前一个邻接结点的下标，来获取下一个邻接结点
+    public int getNextNeighbor(int v1, int v2){
+        for(int j = v2+1;j < vertextList.size();j++){
+            if(edges[v1][j]>0){
+                return j;
+            }
+        }
+        return -1;
+    }
+    //深度优先遍历算法
+    //i,第一次就为0
+    private void dfs(boolean[] isVisited,int i){
+        //访问该结点，输出
+        System.out.print(getValueByIndex(i)+"->");
+        //将该结点设置为已访问
+        isVisited[i] = true;
+        int w = getFirstNeighbor(i);//查找结点i第一个邻接点w
+        while (w != -1){//说明有邻接结点
+            if(!isVisited[w]){
+                dfs(isVisited,w);
+            }
+            //如果w被访问过
+            w = getNextNeighbor(i,w);//
+        }
+    }
+    //对dfs进行重载
+    public void dfs(){
+        //遍历所有结点，进行dfs[回溯]
+        for(int i = 0;i < getNumOfVertext();i++){
+            if(!isVisited[i]){
+                dfs(isVisited,i);
+            }
+        }
+    }
+
+
+
+    //插入结点
+    public void insertVertex(String vertext){
+        vertextList.add(vertext);
+    }
+    //添加边
+    //v1表示点的下标,第几个顶点"A"-"B" "A"->0,"B"->1
+    //v2第二个顶点对应的下标
+    //weight 表示权值
+    public void insertEdge(int v1,int v2, int weight){
+        edges[v1][v2] = weight;
+        edges[v2][v1] = weight;
+        numOfEdges++;
+    }
+}
